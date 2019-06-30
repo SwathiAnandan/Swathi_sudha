956004845654a19b8282a6252a4eb9cee7e1a750
SwathiAnandan <52286779+SwathiAnandan@users.noreply.github.com>
2019-07-01 00:19:06 +0530

    Update Mini-Max Sum.java

--- Hacker_Rank/problem solving/Mini-Max Sum.java
+++ Hacker_Rank/problem solving/Mini-Max Sum.java
@@ -4,15 +4,18 @@ public class Solution {
     static void miniMaxSum(int[] arr) {
         Arrays.sort(arr);
         int l=arr.length;
-        int max=0,min=0;
+        long  max=0,min=0;
         for(int i=0;i<l;i++)
         {
-            if(arr[i]!=arr[0])
+            if(i!=0)
             max+=arr[i];
-            if(arr[i]!=arr[l-1])
+            if(i!=(l-1))
             min+=arr[i];
         }
       System.out.println(min+" "+max);
 
 
     }
+    
+    
+    
