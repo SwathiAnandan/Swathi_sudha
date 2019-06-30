e58a044005a73825780b4461664176df451452a7
SwathiAnandan <52286779+SwathiAnandan@users.noreply.github.com>
2019-06-30 21:56:05 +0530

    Update Diagonal Difference.java

--- Hacker_Rank/problem solving/Diagonal Difference.java
+++ Hacker_Rank/problem solving/Diagonal Difference.java
@@ -1,16 +1,12 @@
 class Result {
 
     public static int diagonalDifference(List<List<Integer>> arr) {
-   int d=0,d1=0,d2=0;
-     for (int i = 0; i < arr.size(); i++) { 
-            for (int j = 0; j < arr.get(i).size(); j++) { 
-                if(i==j)
-                d1+=arr.get(i).get(j); 
-                if((j==(arr.size()-i))||((j==(arr.size()/2))&&(i==(arr.size()/2))))
-                d2+=arr.get(i).get(j); 
-            } 
-    }
-    System.out.println(d1);
- return Math.abs(d1-d2);
-}
+     int d=0,d1=0,d2=0;
+        for (int i = 0; i < arr.size(); i++) { 
+        d1 += arr.get(i).get(i); 
+        d2 += arr.get(i).get(arr.size() - i - 1);         
+    } 
+    d =d1-d2;
+
+ return Math.abs(d);
 }
