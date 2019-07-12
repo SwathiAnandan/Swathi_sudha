82a29255e02fa86887b250fbe11df4c9c0ab2674
SwathiAnandan <52286779+SwathiAnandan@users.noreply.github.com>
2019-07-12 15:27:10 +0530

    Create fibonacci.java

---
+++ TCS/REPEATED QUESTIONS/fibonacci.java
@@ -0,0 +1,16 @@
+class Fibonacci{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i,s=1;    
+  Scanner sc=new Scanner(System.in);
+ int n=sc.nextInt();
+ for(i=1;i<n;++i)
+ {    
+  n3=n1+n2;    
+  s=s+(n3*n3); 
+  n1=n2;    
+  n2=n3;    
+ }  
+ System.out.println(s);
+  
+}}  
