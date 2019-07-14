b7ab40221e39d263f36bb668ce62d8cad55a2691
SwathiAnandan <52286779+SwathiAnandan@users.noreply.github.com>
2019-07-14 18:34:54 +0530

    Update factorialdivisblebysumofsquares.java

--- TCS/REPEATED QUESTIONS/factorialdivisblebysumofsquares.java
+++ TCS/REPEATED QUESTIONS/factorialdivisblebysumofsquares.java
@@ -1,19 +1,19 @@
 //given a integer n, the task is the check the factorial of the value n is divisble by the sum of the squares of the n natual numbers 
 
-class FactorialExample{  
+
+import java.util.*;
+class Main{  
  public static void main(String args[]){  
-  int i,fact=1,s=0;  
-  int number=5;//It is the number to calculate factorial    
-  for(i=1;i<=number;i++){    
+  int i,fact=1,s=0; 
+  Scanner sc= new Scanner(System.in);
+  int n=sc.nextInt();//It is the number to calculate factorial    
+  for(i=1;i<=n;i++){    
       fact=fact*i;  
    s+=i*i;
   }    
-  if(f%s==0)
+  if(fact%s==0)
   System.out.println("yes ");  
   else
     System.out.println("no");   
  }  
 }  
-
-
-
