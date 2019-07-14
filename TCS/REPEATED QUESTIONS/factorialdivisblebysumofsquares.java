201b0c40012912ab8c19d8afa59f9407bcca0aa5
SwathiAnandan <52286779+SwathiAnandan@users.noreply.github.com>
2019-07-14 18:27:32 +0530

    Update factorialdivisblebysumofsquares.java

--- TCS/REPEATED QUESTIONS/factorialdivisblebysumofsquares.java
+++ TCS/REPEATED QUESTIONS/factorialdivisblebysumofsquares.java
@@ -2,11 +2,18 @@
 
 class FactorialExample{  
  public static void main(String args[]){  
-  int i,fact=1;  
+  int i,fact=1,s=0;  
   int number=5;//It is the number to calculate factorial    
   for(i=1;i<=number;i++){    
-      fact=fact*i;    
+      fact=fact*i;  
+   s+=i*i;
   }    
-  System.out.println("Factorial of "+number+" is: "+fact);    
+  if(f%s==0)
+  System.out.println("yes ");  
+  else
+    System.out.println("no");   
  }  
 }  
+
+
+
