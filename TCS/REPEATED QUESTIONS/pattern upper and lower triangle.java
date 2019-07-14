c7a2f62230ba4ec8c22a5ded710da53ff00119f0
SwathiAnandan <52286779+SwathiAnandan@users.noreply.github.com>
2019-07-14 17:32:40 +0530

    Update pattern upper and lower triangle.java

--- TCS/REPEATED QUESTIONS/pattern upper and lower triangle.java
+++ TCS/REPEATED QUESTIONS/pattern upper and lower triangle.java
@@ -63,3 +63,48 @@ public class Main
     
         }
 }
+
+OTHER SOLUTION
+
+AUTHOR:KRISHNA DEEPTHI 
+
+public class Main
+{
+public static void main(String[] args) {
+		int i,j,fstar,bstar,spaces;
+		int n=5;
+		for(i=0;i<n;i++) {
+		 fstar=i+1;
+		 bstar=i+1;
+		 spaces=2*n-(fstar+bstar);
+		 for(j=0;j<fstar;j++) {
+			 System.out.print("*");
+		 }
+			 for(j=0;j<spaces;j++) {
+				 System.out.print(" ");
+			 }
+				 for(j=0;j<bstar;j++) {
+					 System.out.print("*");
+				 }
+			System.out.println();	 
+		}
+			for(i=0;i<n;i++) {
+					 fstar=n-i;
+					 bstar=n-i;
+					 spaces=2*n-(fstar+bstar);
+				
+			for(j=1;j<fstar;j++) {
+				 System.out.print("*");
+			}
+				 for(j=1;j<spaces+2;j++) {
+					 System.out.print(" ");
+				 }
+					 for(j=1;j<bstar;j++) {
+						 System.out.print("*");
+					 }
+	
+			System.out.println();
+
+		}
+	}
+	}
